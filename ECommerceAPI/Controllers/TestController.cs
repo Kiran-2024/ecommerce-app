9015bf27b2d200911f1629849c4f20a61b8abb2a
Kiran <kirannallakantam2002@gmail.com>
2026-05-20 00:48:48 +0530

    Day-2: Database setup, Tables created, ADO.NET DatabaseHelper configured

---
+++ ECommerceAPI/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using ECommerceAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace ECommerceAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly DatabaseHelper _dbHelper;
+
+        public TestController(DatabaseHelper dbHelper)
+        {
+            _dbHelper = dbHelper;
+        }
+
+        [HttpGet("connection")]
+        public IActionResult TestConnection()
+        {
+            try
+            {
+                using var conn = _dbHelper.GetConnection();
+                conn.Open();
+                return Ok("✅ Database Connected Successfully!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"❌ Connection Failed: {ex.Message}");
+            }
+        }
+    }
+}
